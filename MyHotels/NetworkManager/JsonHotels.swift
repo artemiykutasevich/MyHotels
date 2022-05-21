f3726143eef5729ad31047d2ff589036a55b057e
NikitaMedush <97745346+NikitaMedush@users.noreply.github.com>
2022-05-21 13:11:07 +0300

    create fake json

---
+++ MyHotels/NetworkManager/JsonHotels.swift
@@ -0,0 +1,126 @@
+//
+//  JsonHotels.swift
+//  MyHotels
+//
+//  Created by admin on 21.05.22.
+//
+
+let jsonHotels = """
+[{
+  "name" : "The Legian Seminyak",
+  "rating" : 4,
+  "visitors" : [{
+        "name" : "Artem LoveKutit",
+        "rating": 4,
+        "photo": "https://avatars.githubusercontent.com/u/82845071?v=4"
+  },
+  {
+        "name" : "Nikita Meduza",
+        "rating": 4,
+        "photo": "https://avatars.githubusercontent.com/u/97745346?v=4"
+  },
+  {
+        "name" : "Zhenya and Maloi",
+        "rating": 4,
+        "photo": "https://static.wikia.nocookie.net/swfanon/images/4/46/Тони_старк_3.jpg/revision/latest/top-crop/width/360/height/450?cb=20210406190258&path-prefix=ru"
+    }],
+
+  "country" : "Indonesia",
+  "city" : "Jalan Kayu Aya",
+  "price" : 419,
+  "image" : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/b4/e9/11/the-legian-bali-the-beach.jpg?w=2000&h=-1&s=1",
+  "description" : "Finding the perfect romantic hotel in Seminyak should not be a difficult task. Welcome to Legian Bali - Gm Hotel, a great option for accommodating travelers like you.",
+  "category" : "Ocean-sea",
+  "location" : {
+    "latitude" : "-8.684339706316733",
+    "longitude" : "115.15332472671795"
+  }
+},
+{
+  "name" : "Hilton Batumi",
+  "rating" : 5,
+  "visitors" : [{
+        "name" : "Artem LoveKutit",
+        "rating": 5,
+        "photo": "https://avatars.githubusercontent.com/u/82845071?v=4"
+},
+    {
+        "name" : "Nikita Meduza",
+        "rating": 5,
+        "photo": "https://avatars.githubusercontent.com/u/97745346?v=4"
+        },
+    {
+        "name" : "Zhenya and Maloi",
+        "rating": 5,
+        "photo": "https://static.wikia.nocookie.net/swfanon/images/4/46/Тони_старк_3.jpg/revision/latest/top-crop/width/360/height/450?cb=20210406190258&path-prefix=ru"
+    }],
+  "country" : "Georgia",
+  "city" : "Batumi",
+  "price" : 308,
+  "image" : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/5b/a5/21/exterior-of-the-hotel.jpg?w=1400&h=-1&s=1",
+  "description" : "Hilton Batumi is an excellent choice for guests of Batumi, an elite atmosphere and many useful services will make your stay here very pleasant.",
+  "category" : "Ocean-sea",
+  "location" : {
+    "latitude" : "41.64929011825348",
+    "longitude" : "41.62576732679025"
+  }
+  },
+{
+  "name" : "DoubleTree by Hilton Hotel Minsk",
+  "rating" : 4,
+  "visitors" : [{
+        "name" : "Artem LoveKutit",
+        "rating": 4,
+        "photo": "https://avatars.githubusercontent.com/u/82845071?v=4"
+},
+    {
+        "name" : "Nikita Meduza",
+        "rating": 4,
+        "photo": "https://avatars.githubusercontent.com/u/97745346?v=4"
+        },
+    {
+        "name" : "Zhenya and Maloi",
+        "rating": 4,
+        "photo": "https://static.wikia.nocookie.net/swfanon/images/4/46/Тони_старк_3.jpg/revision/latest/top-crop/width/360/height/450?cb=20210406190258&path-prefix=ru"
+    }],
+  "country" : "Belarus",
+  "city" : "Minsk",
+  "price" : 350,
+  "image" : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/23/07/8d/ac/exterior.jpg?w=1100&h=-1&s=1",
+  "description" : "DoubleTree By Hilton Minsk is located in the heart of Minsk, in an exclusive shopping and entertainment complex on Pobediteley Avenue. Within walking distance - the Old Town, museums and theaters, business centers and exhibition complex, Frunzenskaya and Nemiga metro stations. There is a railway station nearby, 45 km to Minsk airport. You will have a warm welcome with DoubleTree by Hilton signature cookies. Admire the view of the Old Town and the embankment of the Svisloch River through the large windows of your room.",
+  "category" : "Ocean-sea",
+  "location" : {
+    "latitude" : "53.90828417986906",
+    "longitude" : "27.548129646931393"
+  }
+  },
+{
+  "name" : "Ibis Styles Tbilisi Center",
+  "rating" : 5,
+  "visitors" : [{
+        "name" : "Artem LoveKutit",
+        "rating": 4,
+        "photo": "https://avatars.githubusercontent.com/u/82845071?v=4"
+},
+    {
+        "name" : "Nikita Meduza",
+        "rating": 5,
+        "photo": "https://avatars.githubusercontent.com/u/97745346?v=4"
+        },
+    {
+        "name" : "Zhenya and Maloi",
+        "rating": 5,
+        "photo": "https://static.wikia.nocookie.net/swfanon/images/4/46/Тони_старк_3.jpg/revision/latest/top-crop/width/360/height/450?cb=20210406190258&path-prefix=ru"
+    }],
+  "country" : "Georgia",
+  "city" : "Tbilisi",
+  "price" : 91,
+  "image" : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/9f/8e/19/exterior-view.jpg?w=1100&h=-1&s=1",
+  "description" : "Looking for a place to stay in Tbilisi? You don't have to search anymore. The family-run Ibis Styles Tbilisi Center Hotel will allow you to get to know Tbilisi in the best possible way.The rooms are equipped with flat-screen TV, air conditioning and a minibar, guests can be online at any time thanks to free Wi-Fi. Here you can have a good rest and regain your strength. Ibis Styles Tbilisi Center offers 24-hour reception, a rooftop terrace and room service. Guests of Ibis Styles Tbilisi Center also have a free breakfast.",
+  "category" : "Mountains",
+  "location" : {
+    "latitude" : "41.69223539079334",
+    "longitude" : "44.80147482722787"
+  }
+}]
+"""
