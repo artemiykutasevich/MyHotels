fa32589801603c03d7c69d6b86235a4480eda411
Artem Kutasevich <artemiy.kutasevich@gmail.com>
2022-06-09 23:14:57 +0300

    add fonts and continue work on AuthorizationViewController

--- MyHotels/Views/AuthorizationViewController.swift
+++ MyHotels/Views/AuthorizationViewController.swift
@@ -13,33 +13,69 @@ class AuthorizationViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         
+        // MARK: - Title
+        
         let label = UILabel()
-        label.text = "Login to your account"
+        label.text = "Login to your \nAccount"
+        label.font = UIFont(name: ProjectFonts.titleFont, size: ProjectFonts.titleFontSize)
+        label.numberOfLines = 2
         view.addSubview(label)
         
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -200).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -250).isActive = true
         label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
         label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
-        label.heightAnchor.constraint(equalToConstant: 56).isActive = true
+        label.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        // MARK: - Email
+        
+        let mailImageView = UIImageView(frame: CGRect(x: 8.0, y: 8.0, width: 24.0, height: 24.0))
+        let mailImage = UIImage(systemName: "mail")
+        mailImageView.image = mailImage
+        mailImageView.contentMode = .scaleAspectFit
+        
+        let mailView = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        mailView.addSubview(mailImageView)
         
         let emailTextField = UITextField()
-        emailTextField.backgroundColor = Colors.textFieldBackground
+        emailTextField.backgroundColor = ProjectColors.textFieldBackgroundColor
+        emailTextField.textColor = ProjectColors.textFieldForegroundColor
         emailTextField.layer.cornerRadius = 14
         emailTextField.placeholder = "email"
+        
+        emailTextField.leftViewMode = .always
+        emailTextField.leftView = mailView
+        emailTextField.tintColor = ProjectColors.textFieldForegroundColor
+        
         view.addSubview(emailTextField)
         
         emailTextField.translatesAutoresizingMaskIntoConstraints = false
-        emailTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -150).isActive = true
+        emailTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -130).isActive = true
         emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
         emailTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
         emailTextField.heightAnchor.constraint(equalToConstant: 56).isActive = true
         
+        // MARK: - Password
+        
+        let passwordImageView = UIImageView(frame: CGRect(x: 8.0, y: 8.0, width: 24.0, height: 24.0))
+        let passwordImage = UIImage(systemName: "lock")
+        passwordImageView.image = passwordImage
+        passwordImageView.contentMode = .scaleAspectFit
+        
+        let passwordView = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        passwordView.addSubview(passwordImageView)
+        
         let passwordTextField = UITextField()
-        passwordTextField.backgroundColor = Colors.textFieldBackground
+        passwordTextField.backgroundColor = ProjectColors.textFieldBackgroundColor
+        passwordTextField.textColor = ProjectColors.textFieldForegroundColor
         passwordTextField.placeholder = "password"
         passwordTextField.layer.cornerRadius = 14
         passwordTextField.isSecureTextEntry = true
+        
+        passwordTextField.leftViewMode = .always
+        passwordTextField.leftView = passwordView
+        passwordTextField.tintColor = ProjectColors.textFieldForegroundColor
+        
         view.addSubview(passwordTextField)
         
         passwordTextField.translatesAutoresizingMaskIntoConstraints = false
@@ -48,10 +84,13 @@ class AuthorizationViewController: UIViewController {
         passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
         passwordTextField.heightAnchor.constraint(equalToConstant: 56).isActive = true
         
+        // MARK: - Sign In Button
+        
         let myButton = UIButton()
-        myButton.backgroundColor = Colors.primaryGreen
+        myButton.backgroundColor = ProjectColors.primaryGreenColor
         myButton.layer.cornerRadius = 28
         myButton.setTitle("Sign in", for: .normal)
+        myButton.titleLabel?.font = UIFont(name: ProjectFonts.titleFont, size: ProjectFonts.bodyFontSize)
         myButton.addTarget(self, action: #selector(authorizationAction), for: .touchUpInside)
         view.addSubview(myButton)
         
